da015769f4b196e27518114ac5eaa79a046b0828
AnimeshJha97 <AnimeshJha97@repo.github.com>
2021-03-05 01:02:43 +0530

    Second Edit

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var app = express();
+const app = express();
+
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+
+    const personObj = {
+        fname : "Animesh",
+        lname : "Jha", 
+        id : 1000
+    }
+    
+    console.log("first name : ", req.query.fname);
+    console.log("last name : ", req.query.lname);
+    
+    res.send(personObj);
+})
+
+app.post('/start', (req, res) => {
+    // console.log("first name : ", req.query.fname);
+    // console.log("last name : ", req.query.lname);
+
+    // const personObj = {
+    //     fname : "Animesh",
+    //     lname : "Jha"
+    // }
+    res.send("Post Request");
+})
+
+
 app.listen(3000, 'localhost', ()=>{
     console.log("App running on port 3000");
 });
